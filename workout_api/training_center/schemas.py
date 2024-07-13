a03838be5792f0b1ff1401e1fa56a3a9db855b67
Gabriel Ezequiel <gabrielzcastro@hotmail.com>
2024-07-12 22:00:51 -0300

    Add training center controller and routes

--- workout_api/training_center/schemas.py
+++ workout_api/training_center/schemas.py
@@ -1,8 +1,14 @@
 from typing import Annotated
-from pydantic import Field
+from pydantic import Field, UUID4
 from workout_api.contrib.schemas import BaseSchema
 
-class TrainingCenter(BaseSchema):
+class TrainingCenterIn(BaseSchema):
     name: Annotated[str, Field(description="The name of the training center", example="Training Center", max_length=20)]
     address: Annotated[str, Field(description="The address of the training center", example="Street, 123", max_length=60)]
     owner: Annotated[str, Field(description="The owner of the training center", example="John Doe", max_length=30)]
+
+class TrainingCenterAthlete(BaseSchema):
+    nome: Annotated[str, Field(description="The name of the training center", example="Training Center", max_length=20)]
+
+class TrainingCenterOut(TrainingCenterIn):
+    id: Annotated[UUID4, Field(description="The identifier of the training center")]
